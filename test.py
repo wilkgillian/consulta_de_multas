3070ca59fca45718cb38630a7ad9d568065a817c
Wilk Gillian Rosa da Silva <wilk.silva@mt.senac.br>
2022-07-27 12:46:00 -0300

    try

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from datetime import datetime
+import asyncio
 from playwright.sync_api import sync_playwright
 import pandas as pd
-from sqlalchemy import false
+from bs4 import BeautifulSoup
 
 
 dataAtual = datetime.today()
@@ -18,29 +19,50 @@ planilha.to_excel("C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xls
 planilha_formatada = pd.read_excel("C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xlsx")
 for index,row in planilha_formatada.iterrows():
     with sync_playwright() as p:
-      browser = p.chromium.launch(headless=False)
+      browser = p.chromium.launch(headless=False, timeout=5000)
       context = browser.new_context()
       page = context.new_page()
       page.goto("https://www.detran.mt.gov.br/")
-      placa = row["PLACA"]
-      renavan = row["RENAVAN"]
-      page.locator('.closer').click()
       time.sleep(1)
-      page.locator('#input_placa').fill(str(placa))
-      page.locator('#input_renavam').fill(str(renavan).replace(".0",""))
-      page.locator('//*[@id="formVeiculo"]/div[4]/input[2]').click()
-      time.sleep(5)
-    #   page.goto("https://internet.detrannet.mt.gov.br/ConsultaVeiculo.asp")
-    #   time.sleep(5)
-      def handle_page(page):
-       page.wait_for_load_state()
-
-       context.on("page", handle_page)
-    #   page.locator('//*[@id="cmbTipoDebito"]').click()
-      print(page.title())
-      time.sleep(5)
-      browser.close()
-#     driver = webdriver.Chrome()
+      try:
+        placa = row["PLACA"]
+        renavan = row["RENAVAN"]
+        page.locator('.closer').click()
+        time.sleep(1)
+        page.locator('#input_placa').fill(str(placa))
+        page.locator('#input_renavam').fill(str(renavan).replace(".0",""))
+        time.sleep(1)
+        with context.expect_event("page") as event_info:
+          page.locator('//*[@id="formVeiculo"]/div[4]/input[2]').click()
+        newTab = event_info.value
+        try: 
+          select = newTab.locator('//*[@id="cmbTipoDebito"]').click()
+          time.sleep(1)
+          pyautogui.press('down')
+          pyautogui.press('down')
+          time.sleep(1)
+          pyautogui.press('enter')
+          # newTab.locator('select#cmbTipoDebito').select_option('Multas')
+          time.sleep(2)
+          # select.locator('//*[@id="cmbTipoDebito"]/option[2]').click()
+          # select.select_option('Multas')
+          # locator('//*[@id="cmbTipoDebito"]').click()
+          print("Existem dados")
+          # tabela = newTab.locator('//*[@id="Integral"]/table/tbody')
+          # content = tabela.get_attribute('outerHtml')
+          # soup = BeautifulSoup(content, 'html.parser')
+          # print(tabela)
+          # time.sleep(1)
+          browser.close()
+        except: 
+          print("Não existem dados")
+          continue 
+        browser.close()
+      except:
+        print("Consulta concluída")
+        continue
+      browser.close()   
+# driver = webdriver.Chrome()
 #     driver.get("https://www.detran.mt.gov.br/")
 #     time.sleep(2)
 #     assert "Início - DETRAN" in driver.title
