567cd1985b0bc9e44dd9f6059ca0180b8d1d2399
Wilk Gillian Rosa da Silva <wilk.silva@mt.senac.br>
2022-08-04 13:00:20 -0300

    openpyxl

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
-from numbers import Integral
-from operator import iconcat
-from unittest import result
 import pyautogui
 import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from datetime import datetime
-import urllib.request
 from playwright.sync_api import sync_playwright
 import pandas as pd
 from bs4 import BeautifulSoup
+import re
+import openpyxl
 
 
 dataAtual = datetime.today()
@@ -57,37 +51,20 @@ for index,row in planilha_formatada.iterrows():
           print(len(debitos))
           print(debitos)
           iContent=0
-          i = 0
+          book = openpyxl.load_workbook(filename="C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xlsx")
+          book.create_sheet("Multas")
+          page_multas = book['Multas']
           while iContent < len(debitos):
             print('\n ---- entrou no while ----\n')
             results = debitos[iContent].contents[1].text
-            # print("\n ----|||||||------ \nDebito n° "+i+" -> "+results+"")
             print(iContent)
-            # print(i)
-            print(results)
+            if re.search("Licenciamento", results):
+                print("Não é uma multa")
+            else:
+                page_multas.append([placa, renavan, results])
+                book.save(filename="C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xlsx")
+                print(results)
             iContent+=1
-            # i+=1
-            # iDebito+=1
-          # # content = itens.text.strip()
-          # print(itens)
-          # debitos = itens.find_all('td', attrs={'class': 'HeaderGrid'})
-          # with pd.ExcelWriter("C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xlsx") as writer:
-          #   to_excel(writer)
-          # for debito in debitos:
-          #   ocorrencia = debito.find('td', attrs={}).get_text().strip()
-
-          #   print('\n ---------- ||||||| --------- \n')
-          #   print(ocorrencia)
-
-          # convertItens = itens.inner_html('td')
-          # print('\n\n\n ---------- ||||||| --------- \n\n\n')
-          # print(convertItens)
-          # newSoup =  BeautifulSoup(convertItens, 'html.parser')
-          # print('\n --- novo soup criado --- \n')
-          # print(convertItens)
-          # ocorrencias =  newSoup.find('td', attrs={})
-          # print('\n --- ocorrencias localizadas --- \n')
-          # print(ocorrencias)
           browser.close()
         except: 
           print("Não existem dados")
@@ -98,25 +75,3 @@ for index,row in planilha_formatada.iterrows():
 
         continue
       browser.close()   
-# driver = webdriver.Chrome()
-#     driver.get("https://www.detran.mt.gov.br/")
-#     time.sleep(2)
-#     assert "Início - DETRAN" in driver.title
-#     driver.maximize_window
-#     openSystem = driver.find_element(By.XPATH, "//*[@id='myPopup']/span").click()
-#     time.sleep(2)
-#     placa = row["PLACA"]
-#     renavan = row["RENAVAN"]
-#     driver.find_element(By.ID, "input_placa").send_keys(placa)
-#     driver.find_element(By.ID, "input_renavam").send_keys(int(renavan))
-#     time.sleep(1)
-#     driver.find_element(By.XPATH, "//*[@id='formVeiculo']/div[4]/input[2]").click()
-#     time.sleep(5)
-#     print(window)
-#     # select = Select(driver.find_element(By.ID, "cmbTipoDebito"))
-#     # select.select_by_visible_text("Multas").click()
-# # # time.sleep(1)
-# # driver.switch_to.window(driver.window_handles[1])
-# # print("Linha "+str(index)+" Coluna "+str(row)+" \n")
-#     driver.quit
-# # planilha.to_excel("C:/Users/wilk.silva/Downloads/Consulta dia "+formatData+".xlsx")
