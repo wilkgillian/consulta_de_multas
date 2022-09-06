35c256d32c56ba6a999ea011fcb318f477261d7a
wilk.gillian <wilk.silva@mt.senac.br>
2022-09-06 10:39:25 -0300

    finishedd

--- executar-consulta_de_multas.bat
+++ executar-consulta_de_multas.bat
@@ -1,5 +1,6 @@
 call C:\Users\wilk.silva\Anaconda3\Scripts\activate.bat
-cd "C:\PythonCodes\consulta_de_multas"
+cd "C:\Users\wilk.silva\Documents\Automation\consulta_de_multas"
+python hello.py
 python Buscador.py
 python BuscadorDenit.py
 python Condutor.py
